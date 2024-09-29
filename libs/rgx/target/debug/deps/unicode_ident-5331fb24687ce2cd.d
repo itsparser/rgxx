6e6317048f78a82c000471fa7564c22e75a86c44
Vasanth kumar Kalaiselvan <itsparser@gmail.com>
2024-09-29 15:46:24 +0530

    Added Stage code for lib based package change

---
+++ libs/rgx/target/debug/deps/unicode_ident-5331fb24687ce2cd.d
@@ -0,0 +1,8 @@
+/Users/itsparser/Drive/OpenSource/itsparser/rgxx/libs/rgx/target/debug/deps/libunicode_ident-5331fb24687ce2cd.rmeta: /Users/itsparser/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/lib.rs /Users/itsparser/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/tables.rs
+
+/Users/itsparser/Drive/OpenSource/itsparser/rgxx/libs/rgx/target/debug/deps/libunicode_ident-5331fb24687ce2cd.rlib: /Users/itsparser/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/lib.rs /Users/itsparser/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/tables.rs
+
+/Users/itsparser/Drive/OpenSource/itsparser/rgxx/libs/rgx/target/debug/deps/unicode_ident-5331fb24687ce2cd.d: /Users/itsparser/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/lib.rs /Users/itsparser/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/tables.rs
+
+/Users/itsparser/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/lib.rs:
+/Users/itsparser/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/tables.rs:
